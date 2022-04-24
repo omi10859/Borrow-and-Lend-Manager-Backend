128f04e005d938f6bf03a18b94882c45b3db74cc
omkar yadav <omkar.yadav@theletstream.com>
2022-04-24 22:16:01 +0530

    add accounts

--- blm/accounts/urls.py
+++ blm/accounts/urls.py
@@ -1,6 +1,8 @@
 from rest_framework.authtoken import views
 from django.urls import path
+from .views import GetUsers
 
 urlpatterns = [
-    path('api-token-auth/', views.obtain_auth_token)
+    path('api-token-auth/', views.obtain_auth_token),
+    path('get_users/', GetUsers.as_view())
 ]
--- blm/accounts/views.py
+++ blm/accounts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth.models import User
 
-# Create your views here.
+from .serializers import UserSerializer
+
+class GetUsers(generics.ListAPIView):
+    permission_classes = [IsAuthenticated]
+
+    models = User
+    serializer_class = UserSerializer
+
+    def get_queryset(self, *args, **kwargs):
+        return self.models.objects.exclude(id=self.request.user.id)
